130105f8468474cbb926c56ef1bf9e0583058a6d
Arkadiy Sychkov <Z4mmer@gmail.com>
2023-04-25 19:07:04 +0500

    Добавил resources

---
+++ test_hw.py
@@ -0,0 +1,25 @@
+import csv
+import os
+import csv
+
+def test_cvs():
+# TODO оформить в тест, добавить ассерты и использовать универсальный путь
+
+PROJECT_ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
+csv_path = os.path.join(PROJECT_ROOT_PATH, 'resources', 'eggs.csv')
+
+with open(csv_path, 'w') as csvfile:
+    csvwriter = csv.writer(csvfile, delimiter=',')
+    csvwriter.writerow(['Anna', 'Pavel', 'Peter'])
+    csvwriter.writerow(['Alex', 'Serj', 'Yana'])
+
+with open(csv_path) as csvfile:
+    csvreader = csv.reader(csvfile)
+    name = []
+    for row in csvreader:
+        name.append(row)
+        print(row)
+    assert name[0] == ['Anna', 'Pavel', 'Peter']
+    assert name[2] == ['Alex', 'Serj', 'Yana']
+
+
